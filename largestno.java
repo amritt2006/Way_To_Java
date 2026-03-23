9b9bf287ae65224dd21804eeeb6e8ab5f8ef5358
Amrit Raj Singh <amritrajsinghh2006@gmail.com>
2026-03-23 11:59:26 +0530

    add largestno file

---
+++ largestno.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class largestno {
+    public static void main(String[] args) {
+        int a,b,c,max;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter first no : ");
+        
+    }
+}
